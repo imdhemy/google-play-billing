22e8f55d6b8d1da7938670a82d524f7fb001d178
AAA <aaa@AAAs-MacBook-Pro-2.local>
2020-10-14 20:07:34 +0200

    implement time isFuture() & isPast()

--- src/ValueObjects/Time.php
+++ src/ValueObjects/Time.php
@@ -3,16 +3,40 @@
 
 namespace Imdhemy\GooglePlay\ValueObjects;
 
+use DateTime;
 
 final class Time
 {
-    public function isFuture(): bool
-    {
+    /**
+     * @var int
+     */
+    private $timestamp;
 
+    /**
+     * Time constructor
+     *
+     * @param integer $timestamp
+     */
+    public function __construct(int $timestamp)
+    {
+        $this->timestamp = $timestamp;
     }
 
+    /**
+     * @return boolean
+     */
+    public function isFuture(): bool
+    {
+        $now = new DateTime('now');
+        return $this->timestamp > $now->getTimestamp()*1000;
+    }
+
+    /**
+     * @return boolean
+     */
     public function isPast(): bool
     {
-
+        $now = new DateTime('now');
+        return $this->timestamp < $now->getTimestamp()*1000;
     }
 }
