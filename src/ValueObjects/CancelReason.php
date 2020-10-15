ccf647023680d7968a57815a83ff602d5a05b4db
imdhemy <imdhemy@gmail.com>
2020-10-15 20:30:00 +0200

    Update CancelReason.php

--- src/ValueObjects/CancelReason.php
+++ src/ValueObjects/CancelReason.php
@@ -4,7 +4,55 @@
 namespace Imdhemy\GooglePlay\ValueObjects;
 
 
-class CancelReason
+/**
+ * Class CancelReason
+ * @package Imdhemy\GooglePlay\ValueObjects
+ */
+final class CancelReason
 {
+    /**
+     * @var int
+     */
+    private $value;
 
+    /**
+     * CancelReason constructor.
+     * @param int $value
+     */
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return bool
+     */
+    public function cancelledByser(): bool
+    {
+        return $this->value === 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function cancelledBySystem(): bool
+    {
+        return $this->value === 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function replacedByNewSubscription(): bool
+    {
+        return $this->value === 2;
+    }
+
+    /**
+     * @return bool
+     */
+    public function cancelledByDeveloper(): bool
+    {
+        return $this->value === 3;
+    }
 }
