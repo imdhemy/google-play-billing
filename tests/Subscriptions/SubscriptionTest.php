ab79be6dc3a970010f1b4cfb7d415b8b4ef78967
imdhemy <imdhemy@gmail.com>
2020-10-15 21:47:51 +0200

    Update SubscriptionTest.php

--- tests/Subscriptions/SubscriptionTest.php
+++ tests/Subscriptions/SubscriptionTest.php
@@ -10,17 +10,36 @@ use Imdhemy\GooglePlay\Tests\TestCase;
 class SubscriptionTest extends TestCase
 {
     /**
-     * @test
+     * @var Subscription
      */
-    public function test_get_method()
+    private $subscription;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $client = ClientFactory::create([ClientFactory::SCOPE_ANDROID_PUBLISHER]);
         $packageName = 'com.twigano.v2';
         $subscriptionId = 'week_premium';
         $token = 'biakahdmblijpjncbijdicac.AO-J1OyEeH6JlhqDSPVIBxpQhG9iFDyz8cVLNx5uLjmLphKmKWjnKpA4d8Q3aDDGRMc-VxQ7IkPTgnTx37NcCWNBkSmxKi1nKAYmc3CyFw21pgHps4dQmJo';
 
-        $subscription = new Subscription($client, $packageName, $subscriptionId, $token);
-        
-        $this->assertInstanceOf(SubscriptionPurchase::class, $subscription->get());
+        $this->subscription = new Subscription($client, $packageName, $subscriptionId, $token);
+    }
+
+    /**
+     * @test
+     */
+    public function test_get_method()
+    {
+        $this->assertInstanceOf(SubscriptionPurchase::class, $this->subscription->get());
+    }
+
+    /**
+     * @test
+     */
+    public function test_acknowledge()
+    {
+        $this->subscription->acknowledge();
+        $this->assertTrue($this->subscription->get());
     }
 }
