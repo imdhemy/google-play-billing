d32a3ccec4c2d3156874e98dca551a0b00f87942
imdhemy <imdhemy@gmail.com>
2020-10-15 20:10:02 +0200

    Implement PromotionType class

--- src/ValueObjects/PromotionType.php
+++ src/ValueObjects/PromotionType.php
@@ -4,7 +4,50 @@
 namespace Imdhemy\GooglePlay\ValueObjects;
 
 
-class PromotionType
+final class PromotionType
 {
+    /**
+     * @var int|null
+     */
+    private $type;
 
+    /**
+     * @var string|null
+     */
+    private $promotionCode;
+
+    /**
+     * PromotionType constructor.
+     * @param int|null $type
+     * @param string|null $promotionCode
+     */
+    public function __construct(?int $type = null, ?string $promotionCode = null)
+    {
+        $this->type = $type;
+        $this->promotionCode = $promotionCode;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOneTimeCode(): bool
+    {
+        return $this->type === 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVanityCode(): bool
+    {
+        return $this->type === 1;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPromotionCode(): ?string
+    {
+        return $this->promotionCode;
+    }
 }
