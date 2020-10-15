bc55e23f2863f9710d50da35f4e695558e146174
imdhemy <imdhemy@gmail.com>
2020-10-15 19:58:32 +0200

    Implement IntroductoryPriceInfo class

--- src/ValueObjects/IntroductoryPriceInfo.php
+++ src/ValueObjects/IntroductoryPriceInfo.php
@@ -4,7 +4,44 @@
 namespace Imdhemy\GooglePlay\ValueObjects;
 
 
-class IntroductoryPriceInfo
+final class IntroductoryPriceInfo
 {
+    /**
+     * @var string
+     */
+    private $introductoryPriceCurrencyCode;
 
+    /**
+     * @var string
+     */
+    private $introductoryPriceAmountMicros;
+
+    /**
+     * @var string
+     */
+    private $introductoryPricePeriod;
+
+    /**
+     * @var int
+     */
+    private $introductoryPriceCycles;
+
+    /**
+     * IntroductoryPriceInfo constructor.
+     * @param string $introductoryPriceCurrencyCode
+     * @param string $introductoryPriceAmountMicros
+     * @param string $introductoryPricePeriod
+     * @param int $introductoryPriceCycles
+     */
+    public function __construct(
+        string $introductoryPriceCurrencyCode,
+        string $introductoryPriceAmountMicros,
+        string $introductoryPricePeriod,
+        int $introductoryPriceCycles
+    ) {
+        $this->introductoryPriceCurrencyCode = $introductoryPriceCurrencyCode;
+        $this->introductoryPriceAmountMicros = $introductoryPriceAmountMicros;
+        $this->introductoryPricePeriod = $introductoryPricePeriod;
+        $this->introductoryPriceCycles = $introductoryPriceCycles;
+    }
 }
