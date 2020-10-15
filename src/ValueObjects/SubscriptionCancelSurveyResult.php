c662aef0a6eb9ad255bf931da93b737c7fdfa5c6
imdhemy <imdhemy@gmail.com>
2020-10-15 20:58:54 +0200

    Update SubscriptionCancelSurveyResult.php

--- src/ValueObjects/SubscriptionCancelSurveyResult.php
+++ src/ValueObjects/SubscriptionCancelSurveyResult.php
@@ -4,15 +4,48 @@
 namespace Imdhemy\GooglePlay\ValueObjects;
 
 
+use CancelSurveyReason;
+
+/**
+ * Class SubscriptionCancelSurveyResult
+ * @package Imdhemy\GooglePlay\ValueObjects
+ */
 final class SubscriptionCancelSurveyResult
 {
+    /**
+     * @var int
+     */
     private $cancelSurveyReason;
 
+    /**
+     * @var string
+     */
     private $userInputCancelReason;
 
-    public function __construct($cancelSurveyReason, $userInputCancelReason)
+    /**
+     * SubscriptionCancelSurveyResult constructor.
+     * @param int $cancelSurveyReason
+     * @param string|null $userInputCancelReason
+     */
+    public function __construct(int $cancelSurveyReason, ?string $userInputCancelReason = null)
     {
         $this->cancelSurveyReason = $cancelSurveyReason;
         $this->userInputCancelReason = $userInputCancelReason;
+    }
+
+    /**
+     * @return CancelSurveyReason
+     */
+    public function getCancelSurveyReason(): CancelSurveyReason
+    {
+        return new CancelSurveyReason($this->cancelSurveyReason);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUserInputCancelReason(): ?string
+    {
+        return $this->userInputCancelReason;
     }
 }
