a1e24172e7801e878e1a75cabfbfa3ece320d045
imdhemy <imdhemy@gmail.com>
2020-12-18 18:03:54 +0200

    implement IntroductoryPriceInfo faker

--- src/ValueObjects/IntroductoryPriceInfo.php
+++ src/ValueObjects/IntroductoryPriceInfo.php
@@ -5,6 +5,7 @@ namespace Imdhemy\GooglePlay\ValueObjects;
 
 final class IntroductoryPriceInfo
 {
+    const INTRO_PRICE_PERIODS = ['P1W', 'P1M', 'P3M', 'P6M', 'P1Y'];
     /**
      * @var string
      */
@@ -51,5 +52,18 @@ final class IntroductoryPriceInfo
     public static function fromArray(array $introductoryPriceInfo): self
     {
         return new self(...array_values($introductoryPriceInfo));
+    }
+
+    /**
+     * @return static
+     */
+    public static function fake(): self
+    {
+        $currencyCode = Price::CURRENCY_CODES[array_rand(Price::CURRENCY_CODES)];
+        $priceAmmountMicros = mt_rand(1, 100) * 1000;
+        $pricePeriod = self::INTRO_PRICE_PERIODS[array_rand(self::INTRO_PRICE_PERIODS)];
+        $cycles = mt_rand(1, 10);
+        
+        return new self($currencyCode, $priceAmmountMicros, $pricePeriod, $cycles);
     }
 }
