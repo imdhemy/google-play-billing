149bf93fda6397b5ae29c22ce2a89d1da2a7915a
imdhemy <imdhemy@gmail.com>
2020-12-18 17:54:25 +0200

    Implement ConsumptionState fakers

--- src/ValueObjects/ConsumptionState.php
+++ src/ValueObjects/ConsumptionState.php
@@ -3,6 +3,10 @@
 
 namespace Imdhemy\GooglePlay\ValueObjects;
 
+/**
+ * Class ConsumptionState
+ * @package Imdhemy\GooglePlay\ValueObjects
+ */
 final class ConsumptionState
 {
     /**
@@ -25,5 +29,29 @@ final class ConsumptionState
     public function isConsumed(): bool
     {
         return $this->consumed === 1;
+    }
+
+    /**
+     * @return static
+     */
+    public static function fake(): self
+    {
+        return new self(mt_rand(0, 1));
+    }
+
+    /**
+     * @return static
+     */
+    public static function fakeConsumed(): self
+    {
+        return new self(1);
+    }
+
+    /**
+     * @return static
+     */
+    public static function fakeNotConsumed(): self
+    {
+        return new self(0);
     }
 }
