035d73e9919af06a6ab6567175b0b6d5d5e2bcfc
imdhemy <imdhemy@gmail.com>
2020-12-20 02:59:10 +0200

    implement Purchase type fakers

--- src/ValueObjects/PurchaseType.php
+++ src/ValueObjects/PurchaseType.php
@@ -5,6 +5,13 @@ namespace Imdhemy\GooglePlay\ValueObjects;
 
 class PurchaseType
 {
+    const TYPE_TEST = 0;
+    const TYPE_PROMO = 1;
+    const TYPE_REWARDED = 2;
+
+    /**
+     * @var int|null
+     */
     protected $type;
 
     /**
@@ -19,12 +26,12 @@ class PurchaseType
 
     public function isTest(): bool
     {
-        return $this->type === 0;
+        return $this->type === self::TYPE_TEST;
     }
 
     public function isPromo(): bool
     {
-        return $this->type === 1;
+        return $this->type === self::TYPE_PROMO;
     }
 
     /**
@@ -32,6 +39,54 @@ class PurchaseType
      */
     public function isRewarded(): bool
     {
-        return $this->type === 2;
+        return $this->type === self::TYPE_REWARDED;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string)$this->type;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return $this
+     */
+    public static function fake(): self
+    {
+        return new self(mt_rand(self::TYPE_TEST, self::TYPE_REWARDED));
+    }
+
+    /**
+     * @return static
+     */
+    public static function fakeTest(): self
+    {
+        return new self(self::TYPE_TEST);
+    }
+
+    /**
+     * @return static
+     */
+    public static function fakePromo(): self
+    {
+        return new self(self::TYPE_PROMO);
+    }
+
+    /**
+     * @return static
+     */
+    public static function fakeRewarded(): self
+    {
+        return new self(self::TYPE_REWARDED);
     }
 }
