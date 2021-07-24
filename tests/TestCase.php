2e627209111b81ea35d37ddbed4d30c9964e5d2d
imdhemy <imdhemy@gmail.com>
2021-07-24 19:00:00 +0200

    chore(security): mock google play credentials file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,5 @@ namespace Imdhemy\GooglePlay\Tests;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @inheritDoc
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $path = realpath(__DIR__ . '/../google-app-credentials.json');
-        putenv(sprintf("GOOGLE_APPLICATION_CREDENTIALS=%s", $path));
-    }
+    //
 }
