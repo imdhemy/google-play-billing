372f0bb2730159108acb4611552ced23dd68b7ef
imdhemy <imdhemy@gmail.com>
2021-07-24 19:00:17 +0200

    feat(client): make scopes an optional param

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -18,11 +18,11 @@ class ClientFactory
      * @param array $scopes
      * @return Client
      */
-    public static function create(array $scopes): Client
+    public static function create(array $scopes = [self::SCOPE_ANDROID_PUBLISHER]): Client
     {
         $middleware = ApplicationDefaultCredentials::getMiddleware($scopes);
 
-        return self::createFromWithMiddleware($middleware, $scopes);
+        return self::createFromWithMiddleware($middleware);
     }
 
     /**
@@ -31,28 +31,27 @@ class ClientFactory
      * @return Client
      * @throws Exception
      */
-    public static function createWithJsonKey(array $jsonKey, array $scopes): Client
+    public static function createWithJsonKey(array $jsonKey, array $scopes = [self::SCOPE_ANDROID_PUBLISHER]): Client
     {
         $credentials = CredentialsLoader::makeCredentials($scopes, $jsonKey);
         $middleware = new AuthTokenMiddleware($credentials);
 
-        return self::createFromWithMiddleware($middleware, $scopes);
+        return self::createFromWithMiddleware($middleware);
     }
 
     /**
      * @param AuthTokenMiddleware $middleware
-     * @param array $scopes
      * @return Client
      */
-    public static function createFromWithMiddleware(AuthTokenMiddleware $middleware, array $scopes)
+    public static function createFromWithMiddleware(AuthTokenMiddleware $middleware): Client
     {
         $stack = HandlerStack::create();
         $stack->push($middleware);
 
         return new Client([
-            'handler' => $stack,
-            'base_uri' => 'https://www.googleapis.com',
-            'auth' => 'google_auth',
+          'handler' => $stack,
+          'base_uri' => 'https://www.googleapis.com',
+          'auth' => 'google_auth',
         ]);
     }
 }
--- tests/ClientFactoryTest.php
+++ tests/ClientFactoryTest.php
@@ -20,14 +20,21 @@ class ClientFactoryTest extends TestCase
 
     /**
      * @test
+     */
+    public function test_scopes_are_optional()
+    {
+        $this->assertInstanceOf(Client::class, ClientFactory::create());
+    }
+
+    /**
+     * @test
      * @throws Exception
      */
     public function test_it_creates_guzzle_http_client_with_json_key_supplied_as_array()
     {
-        $keyStream = file_get_contents(__DIR__ . '/../google-app-credentials.json');
+        $keyStream = file_get_contents(__DIR__ . '/assets/google-app-credentials.json');
         $jsonKey = json_decode($keyStream, true);
-        $scopes = [ClientFactory::SCOPE_ANDROID_PUBLISHER];
-        $client = ClientFactory::createWithJsonKey($jsonKey, $scopes);
+        $client = ClientFactory::createWithJsonKey($jsonKey);
         $this->assertInstanceOf(Client::class, $client);
     }
 }
