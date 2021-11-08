8c800d6fe5edba2f9c936bb763a5949f592a672c
imdhemy <imdhemy@gmail.com>
2021-11-08 16:10:19 +0300

    chore(test-notification): cover with tests

--- src/DeveloperNotifications/TestNotification.php
+++ src/DeveloperNotifications/TestNotification.php
@@ -4,6 +4,11 @@ namespace Imdhemy\GooglePlay\DeveloperNotifications;
 
 use Imdhemy\GooglePlay\DeveloperNotifications\Contracts\NotificationPayload;
 
+/**
+ * TestNotification class
+ * Test Notification
+ * {@link https://developer.android.com/google/play/billing/rtdn-reference#test}
+ */
 class TestNotification implements NotificationPayload
 {
     /**
@@ -18,6 +23,15 @@ class TestNotification implements NotificationPayload
     public function __construct(string $version)
     {
         $this->version = $version;
+    }
+
+    /**
+     * @param array $attributes
+     * @return static
+     */
+    public static function create(array $attributes): self
+    {
+        return new self($attributes['version']);
     }
 
     /**
