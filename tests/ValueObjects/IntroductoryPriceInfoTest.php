032db5b2b294747a6f4080147b3cebbae9e5f9cc
imdhemy <imdhemy@gmail.com>
2022-03-17 16:33:26 +0300

    chore: introductory price info code refactoring

---
+++ tests/ValueObjects/IntroductoryPriceInfoTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Imdhemy\GooglePlay\Tests\ValueObjects;
+
+use Imdhemy\GooglePlay\Tests\TestCase;
+use Imdhemy\GooglePlay\ValueObjects\IntroductoryPriceInfo;
+
+class IntroductoryPriceInfoTest extends TestCase
+{
+    /**
+     * @var array
+     */
+    private $attributes;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->attributes = [
+            IntroductoryPriceInfo::CURRENCY_CODE => $this->faker->currencyCode(),
+            IntroductoryPriceInfo::AMOUNT_MICROS => $this->faker->randomElement(range(0, 100)),
+            IntroductoryPriceInfo::PRICE_PERIOD => $this->faker->randomElement(
+                IntroductoryPriceInfo::INTRO_PRICE_PERIODS
+            ),
+            IntroductoryPriceInfo::PRICE_CYCLES => $this->faker->randomElement(range(0, 10)),
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function construct()
+    {
+        $introductoryPriceInfo = new IntroductoryPriceInfo(...array_values($this->attributes));
+        $this->assertEquals($this->attributes, $introductoryPriceInfo->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function from_array()
+    {
+        $introductoryPriceInfo = IntroductoryPriceInfo::fromArray($this->attributes);
+        $this->assertInstanceOf(IntroductoryPriceInfo::class, $introductoryPriceInfo);
+    }
+}
