5c4c70fda3b8b106a1c4201e1e9a52be8ea15cd0
imdhemy <imdhemy@gmail.com>
2024-11-09 21:56:17 +0100

    wip: value objects

---
+++ src/Purchase/Subscription/Resource/AutoRenewingPlan.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\Purchase\Subscription\Resource;
+
+/**
+ * Information related to an auto renewing plan.
+ *
+ * @see https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptionsv2#autorenewingplan
+ */
+final class AutoRenewingPlan
+{
+
+}
---
+++ src/ValueObjects/Money.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\ValueObjects;
+
+/**
+ * Represents an amount of money with its currency type.
+ *
+ * @see https://developers.google.com/android-publisher/api-ref/rest/v3/Money
+ */
+final class Money
+{
+
+}
