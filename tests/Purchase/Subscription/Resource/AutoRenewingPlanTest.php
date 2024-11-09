a5709f9de63f8351778b09cf78d6b8d7785b20ef
imdhemy <imdhemy@gmail.com>
2024-11-09 21:56:17 +0100

    wip: value objects

---
+++ tests/Purchase/Subscription/Resource/AutoRenewingPlanTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Purchase\Subscription\Resource;
+
+use Imdhemy\GooglePlay\Purchase\Subscription\Resource\AutoRenewingPlan;
+use PHPUnit\Framework\TestCase;
+
+class AutoRenewingPlanTest extends TestCase
+{
+
+}
