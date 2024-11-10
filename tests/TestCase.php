b4c68483ca8e70eef271102677c49de17f4ff0b2
imdhemy <imdhemy@gmail.com>
2024-11-10 11:19:44 +0100

    wip: SubscriptionItemPriceChangeDetails

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,4 +23,19 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     {
         return json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR);
     }
+
+    protected function todo(string $message): void
+    {
+        $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];
+
+        $warning = sprintf(
+            'Warning: %s in %s on line %d',
+            $message,
+            $caller['file'],
+            $caller['line']
+        );
+        trigger_error($warning, E_USER_WARNING);
+        
+        $this->markTestIncomplete($message);
+    }
 }
