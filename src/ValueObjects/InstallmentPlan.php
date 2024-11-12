a134e021b31913c0bfdaaf4ea7a4fb7ef20d51cc
imdhemy <imdhemy@gmail.com>
2024-11-12 20:02:30 +0100

    wip: implement installment plan

---
+++ src/ValueObjects/InstallmentPlan.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\ValueObjects;
+
+final readonly class InstallmentPlan
+{
+    public function __construct(
+        public int $initialCommittedPaymentsCount,
+        public int $subsequentCommittedPaymentsCount,
+        public int $remainingCommittedPaymentsCount,
+        public ?PendingCancellation $pendingCancellation,
+    ) {
+    }
+}
---
+++ src/ValueObjects/PendingCancellation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Imdhemy\GooglePlay\ValueObjects;
+
+/**
+ * This type has no fields.
+ * This is an indicator of whether there is a pending cancellation on the virtual installment plan. The cancellation
+ * will happen only after the user finished all committed payments.
+ */
+final class PendingCancellation
+{
+}
