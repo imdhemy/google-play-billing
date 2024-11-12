41254219543f97ade8e1e0250ed0bb88c592e2ee
imdhemy <imdhemy@gmail.com>
2024-11-12 20:04:10 +0100

    Update InstallmentPlan.php

--- src/ValueObjects/InstallmentPlan.php
+++ src/ValueObjects/InstallmentPlan.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Imdhemy\GooglePlay\ValueObjects;
 
+/**
+ * Information to a installment plan.
+ *
+ * @see https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptionsv2#installmentplan
+ */
 final readonly class InstallmentPlan
 {
     public function __construct(
