ccddd4c098f87f9acef3a70a0da07aa6779c2892
Dhemy <imdhemy@gmail.com>
2025-06-10 20:49:30 +0300

    chore: implement AutoRenewingPlan

--- src/ValueObjects/AutoRenewingPlan.php
+++ src/ValueObjects/AutoRenewingPlan.php
@@ -9,6 +9,13 @@ namespace Imdhemy\GooglePlay\ValueObjects;
  *
  * @see https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptionsv2#autorenewingplan
  */
-final class AutoRenewingPlan
+final readonly class AutoRenewingPlan
 {
+    public function __construct(
+        public bool $autoRenewEnabled,
+        public Money $recurringPrice,
+        public SubscriptionItemPriceChangeDetails $priceChangeDetails,
+        public InstallmentPlan $installmentDetails,
+    ) {
+    }
 }
