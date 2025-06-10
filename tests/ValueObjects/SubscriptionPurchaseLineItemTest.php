19524512818234ff645b78c5d778d3867a95f2f9
Dhemy <imdhemy@gmail.com>
2025-06-10 22:21:06 +0300

    Update SubscriptionPurchaseLineItemTest.php

--- tests/ValueObjects/SubscriptionPurchaseLineItemTest.php
+++ tests/ValueObjects/SubscriptionPurchaseLineItemTest.php
@@ -9,17 +9,6 @@ use Tests\TestCase;
 
 final class SubscriptionPurchaseLineItemTest extends TestCase
 {
-    private function assertNormalizedDataMatches(array $data, SubscriptionPurchaseLineItem $actual): void
-    {
-        $this->assertSame($data['productId'], $actual->productId);
-        $this->assertSame($data['expiryTime'], $actual->expiryTime->originalValue);
-        $this->assertSame($data['latestSuccessfulOrderId'], $actual->latestSuccessfulOrderId);
-        $this->assertNotNull($actual->autoRenewingPlan);
-        $this->assertNull($actual->prepaidPlan);
-        $this->assertSame($data['offerDetails']['offerTags'], $actual->offerDetails->offerTags);
-        $this->assertSame($data['offerDetails']['basePlanId'], $actual->offerDetails->basePlanId);
-        $this->assertSame($data['offerDetails']['offerId'], $actual->offerDetails->offerId);
-    }
     /** @test */
     public function instantiate_with_auto_renewing_plan(): void
     {
@@ -144,7 +133,7 @@ final class SubscriptionPurchaseLineItemTest extends TestCase
         ];
 
         $actual = $this->normalizer->normalize($data, SubscriptionPurchaseLineItem::class);
-
+        
         $this->assertSame($data['productId'], $actual->productId);
         $this->assertSame($data['expiryTime'], $actual->expiryTime->originalValue);
         $this->assertSame($data['latestSuccessfulOrderId'], $actual->latestSuccessfulOrderId);
