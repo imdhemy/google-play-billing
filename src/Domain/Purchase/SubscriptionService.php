d44d89109079c0295732e80488c12aaf2f63fe1a
Dhemy <imdhemy@gmail.com>
2025-06-13 23:56:55 +0300

    feat(purchase): support revoke subscription v2

--- src/Domain/Purchase/SubscriptionService.php
+++ src/Domain/Purchase/SubscriptionService.php
@@ -6,16 +6,20 @@ namespace Imdhemy\GooglePlay\Domain\Purchase;
 
 use GuzzleHttp\Psr7\Request;
 use Imdhemy\GooglePlay\Domain\NormalizerInterface;
+use Imdhemy\GooglePlay\Domain\SerializerInterface;
+use Imdhemy\GooglePlay\ValueObjects\RevocationContext;
 use Psr\Http\Client\ClientInterface;
 use UnexpectedValueException;
 
 final readonly class SubscriptionService
 {
     private const string GET_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptionsv2/tokens/{token}';
+    private const string REVOKE_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptionsv2/tokens/{token}:revoke';
 
     public function __construct(
         private ClientInterface $client,
         private NormalizerInterface $normalizer,
+        private SerializerInterface $serializer,
     ) {
     }
 
@@ -27,9 +31,41 @@ final readonly class SubscriptionService
             subject: self::GET_ENDPOINT
         );
 
-        $data = $this->doGet(new Request('GET', $uri));
+        $request = new Request(
+            method: 'GET',
+            uri: $uri,
+            headers: [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ]
+        );
+
+        $data = $this->doGet($request);
 
         return $this->normalizer->normalize(data: $data, type: Subscription::class);
+    }
+
+    public function revoke(string $packageName, string $token, RevocationContext $revocationContext): void
+    {
+        $uri = str_replace(
+            search: ['{packageName}', '{token}'],
+            replace: [$packageName, $token],
+            subject: self::REVOKE_ENDPOINT
+        );
+
+        $body = $this->serializer->serialize(data: compact('revocationContext'));
+
+        $request = new Request(
+            method: 'POST',
+            uri: $uri,
+            headers: [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ],
+            body: $body
+        );
+
+        $this->client->sendRequest($request);
     }
 
     private function doGet(Request $request): array
