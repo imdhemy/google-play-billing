d6457973777ab4c51f662074f7ecef37cbf91b22
Dhemy <imdhemy@gmail.com>
2025-06-17 21:40:41 +0300

    feat(purchase): support product consume

--- tests/Domain/Purchase/ProductServiceTest.php
+++ tests/Domain/Purchase/ProductServiceTest.php
@@ -67,4 +67,29 @@ final class ProductServiceTest extends TestCase
             ),
         );
     }
+
+    /** @test */
+    public function consume(): void
+    {
+        $packageName = 'com.some.thing';
+        $productId = 'com.some.thing.inapp1';
+        $token = $this->faker->productToken();
+        $history = [];
+        $client = $this->mockClient([new Response()], $history);
+        $sut = new ProductService(client: $client, serializer: $this->serializer);
+
+        $sut->consume(packageName: $packageName, productId: $productId, token: $token);
+
+        $this->assertClientSentRequest(
+            history: $history,
+            request: new Request(
+                method: 'POST',
+                uri: 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.some.thing/purchases/products/com.some.thing.inapp1/tokens/'.$token.':consume',
+                headers: [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+            ),
+        );
+    }
 }
