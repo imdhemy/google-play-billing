453a04429132f71f717a2ca10c7ee271a1271d65
Dhemy <imdhemy@gmail.com>
2025-06-19 22:41:15 +0200

    feat(purchase): support get product purchase (#177)

--- src/Domain/Purchase/ProductService.php
+++ src/Domain/Purchase/ProductService.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace Imdhemy\GooglePlay\Domain\Purchase;
 
 use GuzzleHttp\Psr7\Request;
+use Imdhemy\GooglePlay\Domain\NormalizerInterface;
 use Imdhemy\GooglePlay\Domain\SerializerInterface;
 use Psr\Http\Client\ClientInterface;
+use UnexpectedValueException;
 
 final readonly class ProductService
 {
     private const string ACKNOWLEDGE_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/products/{productId}/tokens/{token}:acknowledge';
     private const string CONSUME_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/products/{productId}/tokens/{token}:consume';
+    private const string GET_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/products/{productId}/tokens/{token}';
 
     public function __construct(
         private ClientInterface $client,
+        private NormalizerInterface $normalizer,
         private SerializerInterface $serializer,
     ) {
     }
@@ -65,5 +69,39 @@ final readonly class ProductService
         );
 
         $this->client->sendRequest($request);
+    }
+
+    public function get(string $packageName, string $productId, string $token): ProductPurchase
+    {
+        $data = $this->doGet($packageName, $productId, $token);
+
+        return $this->normalizer->normalize(data: $data, type: ProductPurchase::class);
+    }
+
+    private function doGet(string $packageName, string $productId, string $token): array
+    {
+        $uri = str_replace(
+            search: ['{packageName}', '{productId}', '{token}'],
+            replace: [$packageName, $productId, $token],
+            subject: self::GET_ENDPOINT
+        );
+
+        $request = new Request(
+            method: 'GET',
+            uri: $uri,
+            headers: [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ]
+        );
+
+        $response = $this->client->sendRequest($request);
+        $data = \json_decode($response->getBody()->getContents(), true, 512, JSON_PARTIAL_OUTPUT_ON_ERROR);
+
+        if (! is_array($data)) {
+            throw new UnexpectedValueException('Expected response to be an array.');
+        }
+
+        return $data;
     }
 }
