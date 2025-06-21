5ace2fceb55f4401c6db225643d37077fe806776
Dhemy <imdhemy@gmail.com>
2025-06-21 15:55:02 +0300

    chore(rtdn): implement DeveloperNotification

---
+++ src/Infrastructure/Rtdn/DeveloperNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\Infrastructure\Rtdn;
+
+use Imdhemy\GooglePlay\ValueObjects\Time;
+
+/**
+ * @see https://developer.android.com/google/play/billing/rtdn-reference#encoding
+ */
+final readonly class DeveloperNotification
+{
+    public function __construct(
+        public string $version,
+        public string $packageName,
+        public Time $eventTimeMillis,
+        public ?OneTimeProductNotification $oneTimeProductNotification = null,
+        public ?SubscriptionNotification $subscriptionNotification = null,
+        public ?VoidedPurchaseNotification $voidedPurchaseNotification = null,
+        public ?TestNotification $testNotification = null,
+    ) {
+    }
+}
