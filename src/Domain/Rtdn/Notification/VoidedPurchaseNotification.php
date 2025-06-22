68916f94f8c0ef2a33a50a5c18d8e59a2c1b59ad
Dhemy <imdhemy@gmail.com>
2025-06-22 18:15:33 +0300

    feat(rtdn): Implement real-time developer notifications (#182)

--- src/Domain/Rtdn/Notification/VoidedPurchaseNotification.php
+++ src/Domain/Rtdn/Notification/VoidedPurchaseNotification.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace Imdhemy\GooglePlay\Domain\Rtdn\Notification;
 
-final class VoidedPurchaseNotification
+use Imdhemy\GooglePlay\Domain\Rtdn\Type\ProductType;
+use Imdhemy\GooglePlay\Domain\Rtdn\Type\RefundType;
+
+/**
+ * @see https://developer.android.com/google/play/billing/rtdn-reference#voided-purchase
+ */
+final readonly class VoidedPurchaseNotification
 {
+    public function __construct(
+        public string $purchaseToken,
+        public string $orderId,
+        public ProductType $productType,
+        public RefundType $refundType,
+    ) {
+    }
 }
