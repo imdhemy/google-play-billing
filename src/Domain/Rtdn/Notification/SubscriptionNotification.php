29b6a915995a74d512f3da9d577895df77af9978
Dhemy <imdhemy@gmail.com>
2025-06-22 18:11:09 +0300

    feat(rtdn): implement SubscriptionNotification

--- src/Domain/Rtdn/Notification/SubscriptionNotification.php
+++ src/Domain/Rtdn/Notification/SubscriptionNotification.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace Imdhemy\GooglePlay\Domain\Rtdn\Notification;
 
-final class SubscriptionNotification
+use Imdhemy\GooglePlay\Domain\Rtdn\Type\SubscriptionNotificationType;
+
+/**
+ * @see https://developer.android.com/google/play/billing/rtdn-reference#sub
+ */
+final readonly class SubscriptionNotification
 {
+    public function __construct(
+        public string $version,
+        public SubscriptionNotificationType $notificationType,
+        public string $purchaseToken,
+    ) {
+    }
 }
