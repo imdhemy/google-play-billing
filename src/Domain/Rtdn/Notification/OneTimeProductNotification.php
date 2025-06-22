b34d9f4536b2e5cc942ed1bff2548319c601e484
Dhemy <imdhemy@gmail.com>
2025-06-22 18:09:04 +0300

    feat(rtdn): implement OneTimeProductNotification

--- src/Domain/Rtdn/Notification/OneTimeProductNotification.php
+++ src/Domain/Rtdn/Notification/OneTimeProductNotification.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Imdhemy\GooglePlay\Domain\Rtdn\Notification;
 
-final class OneTimeProductNotification
+use Imdhemy\GooglePlay\Domain\Rtdn\Type\OneTimeProductNotificationType;
+
+/**
+ * @see https://developer.android.com/google/play/billing/rtdn-reference#one-time
+ */
+final readonly class OneTimeProductNotification
 {
+    public function __construct(
+        public string $version,
+        public OneTimeProductNotificationType $notificationType,
+        public string $purchaseToken,
+        public string $sku,
+    ) {
+    }
 }
