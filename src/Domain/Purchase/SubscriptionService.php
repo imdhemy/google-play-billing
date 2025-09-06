05426f81ab8e511b16527514eb7fff98ed26cd31
EmanFateen <emanfateen@gmail.com>
2025-09-06 17:42:45 +0300

    feat(Subscription): support legacy cancel

--- src/Domain/Purchase/SubscriptionService.php
+++ src/Domain/Purchase/SubscriptionService.php
@@ -16,7 +16,8 @@ final readonly class SubscriptionService
 {
     private const string GET_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptionsv2/tokens/{token}';
     private const string REVOKE_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptionsv2/tokens/{token}:revoke';
-    private const string ACKNOWLEDGE_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptions/tokens/{token}:acknowledge';
+    private const string LEGACY_ACKNOWLEDGE_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptions/tokens/{token}:acknowledge';
+    private const string LEGACY_CANCEL_ENDPOINT = 'https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{packageName}/purchases/subscriptions/tokens/{token}:cancel';
 
     public function __construct(
         private ClientInterface $client,
@@ -58,7 +59,7 @@ final readonly class SubscriptionService
         $uri = str_replace(
             search: ['{packageName}', '{token}'],
             replace: [$packageName, $token],
-            subject: self::ACKNOWLEDGE_ENDPOINT
+            subject: self::LEGACY_ACKNOWLEDGE_ENDPOINT
         );
 
         $request = new Request(
@@ -69,6 +70,27 @@ final readonly class SubscriptionService
                 'Content-Type' => 'application/json',
             ],
             body: $this->serializer->serialize(data: ['developerPayload' => $developerPayload])
+        );
+
+        $this->client->sendRequest($request);
+    }
+
+    public function legacyCancel(string $packageName, string $token, string $cancellationType): void
+    {
+        $uri = str_replace(
+            search: ['{packageName}', '{token}'],
+            replace: [$packageName, $token],
+            subject: self::LEGACY_CANCEL_ENDPOINT
+        );
+
+        $request = new Request(
+            method: 'POST',
+            uri: $uri,
+            headers: [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ],
+            body: $this->serializer->serialize(data: compact('cancellationType'))
         );
 
         $this->client->sendRequest($request);
