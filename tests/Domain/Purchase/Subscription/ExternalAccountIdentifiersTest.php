0dd2eed352660393b8fa8422c872337bd9102f98
Daniel Bernhardt <daniel.bernhardt@preventicus.com>
2025-10-28 08:12:01 +0100

    add test for ExternalAccountIdentifiers with optional account ids

--- tests/Domain/Purchase/Subscription/ExternalAccountIdentifiersTest.php
+++ tests/Domain/Purchase/Subscription/ExternalAccountIdentifiersTest.php
@@ -25,4 +25,20 @@ final class ExternalAccountIdentifiersTest extends TestCase
         $this->assertEquals($data['obfuscatedExternalAccountId'], $actual->obfuscatedExternalAccountId);
         $this->assertEquals($data['obfuscatedExternalProfileId'], $actual->obfuscatedExternalProfileId);
     }
+
+    /** @test */
+    public function instantiation_without_external_account_id(): void
+    {
+        $data = [
+            // externalAccountId and obfuscatedExternalProfileId are optional
+            'obfuscatedExternalAccountId' => $this->faker->uuid(),
+        ];
+
+        $actual = $this->normalizer->normalize($data, ExternalAccountIdentifiers::class);
+
+        $this->assertInstanceOf(ExternalAccountIdentifiers::class, $actual);
+        $this->assertNull($actual->externalAccountId);
+        $this->assertEquals($data['obfuscatedExternalAccountId'], $actual->obfuscatedExternalAccountId);
+        $this->assertNull($actual->obfuscatedExternalProfileId);
+    }
 }
