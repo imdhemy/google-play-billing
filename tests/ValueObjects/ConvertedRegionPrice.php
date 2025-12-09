bc0ecd9ca28989b213cd0d62eb84aadd9f483bf4
EmanFateen <emanfateen@gmail.com>
2025-12-09 23:52:20 +0200

    chore: introduce converted region price

---
+++ tests/ValueObjects/ConvertedRegionPrice.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ValueObjects;
+
+use Imdhemy\GooglePlay\ValueObjects\Money;
+
+/**
+ * Represents the converted region price.
+ *
+ * @see https://developers.google.com/android-publisher/api-ref/rest/v3/monetization/convertRegionPrices#convertedregionprice
+ */
+final class ConvertedRegionPrice
+{
+    public function __construct(
+        public string $regionCode,
+        public Money $price,
+        public Money $taxAmount,
+    ) {
+    }
+}
---
+++ tests/ValueObjects/ConvertedRegionPriceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ValueObjects;
+
+use Imdhemy\GooglePlay\ValueObjects\Money;
+use Tests\TestCase;
+
+final class ConvertedRegionPriceTest extends TestCase
+{
+    /** @test */
+    public function instantiate(): void
+    {
+        $data = [
+            'regionCode' => $this->faker->countryCode(),
+            'price' => [
+                'currencyCode' => $this->faker->currencyCode(),
+                'units' => (string)$this->faker->randomNumber(5),
+                'nanos' => $this->faker->randomNumber(5),
+            ],
+            'taxAmount' => [
+                'currencyCode' => $this->faker->currencyCode(),
+                'units' => (string)$this->faker->randomNumber(5),
+                'nanos' => $this->faker->randomNumber(5),
+            ],
+        ];
+
+        $actual = $this->normalizer->normalize($data, ConvertedRegionPrice::class);
+
+        $this->assertInstanceOf(ConvertedRegionPrice::class, $actual);
+        $this->assertSame($data['regionCode'], $actual->regionCode);
+        $this->assertInstanceOf(Money::class, $actual->price);
+        $this->assertInstanceOf(Money::class, $actual->taxAmount);
+    }
+}
