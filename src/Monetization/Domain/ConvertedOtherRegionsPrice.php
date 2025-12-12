8f7707925793e1f48e7c0062e0e084b2d5ee07d5
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2025-12-12 19:17:10 +0200

    chore(monetization): introduce `ConvertedOtherRegionsPriceTest`  and add test coverage

---
+++ src/Monetization/Domain/ConvertedOtherRegionsPrice.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\Monetization\Domain;
+
+use Imdhemy\GooglePlay\ValueObjects\Money;
+
+/**
+ * Represents the converted other region price.
+ *
+ * {@link https://developers.google.com/android-publisher/api-ref/rest/v3/monetization/convertRegionPrices#ConvertedOtherRegionsPrice}
+ */
+final readonly class ConvertedOtherRegionsPrice
+{
+    public function __construct(
+        public Money $usdPrice,
+        public Money $eurPrice,
+    ) {}
+}
---
+++ tests/Monetization/Domain/ConvertedOtherRegionsPriceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Monetization\Domain;
+
+use Imdhemy\GooglePlay\Monetization\Domain\ConvertedOtherRegionsPrice;
+use Imdhemy\GooglePlay\ValueObjects\Money;
+use Tests\TestCase;
+
+final class ConvertedOtherRegionsPriceTest extends TestCase
+{
+    /** @test */
+    public function instantiate(): void
+    {
+        $data = [
+            'usdPrice' => [
+                'currencyCode' => 'USD',
+                'units' => '4',
+                'nanos' => 490000000,
+            ],
+            'eurPrice' => [
+                'currencyCode' => 'EUR',
+                'units' => '0',
+                'nanos' => 0,
+            ],
+        ];
+
+        $actual = $this->normalizer->normalize($data, ConvertedOtherRegionsPrice::class);
+
+        $this->assertInstanceOf(ConvertedOtherRegionsPrice::class, $actual);
+        $this->assertEquals(new Money('USD', '4', 490000000), $actual->usdPrice);
+        $this->assertEquals(new Money('EUR', '0', 0), $actual->eurPrice);
+    }
+}
