e32f21bc167721cadab961f2c854eaff991a0843
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2025-12-23 17:55:21 +0200

    Refactor(monetization): Refactor ConvertedPrices test assertions.

--- tests/Monetization/Domain/ConvertedPricesTest.php
+++ tests/Monetization/Domain/ConvertedPricesTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Monetization\Domain;
 
+use Imdhemy\GooglePlay\Monetization\Domain\ConvertedOtherRegionsPrice;
 use Imdhemy\GooglePlay\Monetization\Domain\ConvertedPrices;
+use Imdhemy\GooglePlay\Monetization\Domain\ConvertedRegionPrice;
 use Imdhemy\GooglePlay\ValueObjects\Money;
 use Tests\TestCase;
 
@@ -62,18 +64,20 @@ final class ConvertedPricesTest extends TestCase
         $actual = $this->normalizer->normalize($data, ConvertedPrices::class);
 
         $this->assertInstanceOf(ConvertedPrices::class, $actual);
-
         $this->assertCount(2, $actual->convertedRegionPrices);
-
-        $this->assertEquals(new Money('EUR', '10', 990000000), $actual->convertedRegionPrices['DE']->price);
-        $this->assertEquals(new Money('EUR', '1', 760000000), $actual->convertedRegionPrices['DE']->taxAmount);
-
-        $this->assertEquals(new Money('JPY', '1480', 0), $actual->convertedRegionPrices['JP']->price);
-        $this->assertEquals(new Money('JPY', '135', 0), $actual->convertedRegionPrices['JP']->taxAmount);
-
-        $this->assertEquals(new Money('USD', '9', 990000000), $actual->convertedOtherRegionsPrice->usdPrice);
-        $this->assertEquals(new Money('EUR', '9', 490000000), $actual->convertedOtherRegionsPrice->eurPrice);
-
+        $ConvertedRegionPrice = ['DE' => new ConvertedRegionPrice('DE',
+            new Money('EUR', '10', 990000000),
+            new Money('EUR', '1', 760000000),
+        ), 'JP' => new ConvertedRegionPrice('JP',
+            new Money('JPY', '1480', 0),
+            new Money('JPY', '135', 0),
+        )];
+        $this->assertEquals($ConvertedRegionPrice, $actual->convertedRegionPrices);
+        $convertedOtherRegionsPrice = new ConvertedOtherRegionsPrice(
+            new Money('USD', '9', 990000000),
+            new Money('EUR', '9', 490000000),
+        );
+        $this->assertEquals($convertedOtherRegionsPrice, $actual->convertedOtherRegionsPrice);
         $this->assertEquals('2024/02', $actual->regionVersion->version);
     }
 }
