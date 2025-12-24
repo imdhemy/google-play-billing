6f6919b05ad0b53cf9ec8397903bedcb34b76f63
Dhemy <imdhemy@gmail.com>
2025-12-24 01:33:35 +0100

    chore: simplify test

--- tests/Monetization/Domain/ConvertedPricesTest.php
+++ tests/Monetization/Domain/ConvertedPricesTest.php
@@ -8,6 +8,7 @@ use Imdhemy\GooglePlay\Monetization\Domain\ConvertedOtherRegionsPrice;
 use Imdhemy\GooglePlay\Monetization\Domain\ConvertedPrices;
 use Imdhemy\GooglePlay\Monetization\Domain\ConvertedRegionPrice;
 use Imdhemy\GooglePlay\ValueObjects\Money;
+use Imdhemy\GooglePlay\ValueObjects\RegionsVersion;
 use Tests\TestCase;
 
 final class ConvertedPricesTest extends TestCase
@@ -64,20 +65,25 @@ final class ConvertedPricesTest extends TestCase
         $actual = $this->normalizer->normalize($data, ConvertedPrices::class);
 
         $this->assertInstanceOf(ConvertedPrices::class, $actual);
-        $this->assertCount(2, $actual->convertedRegionPrices);
-        $ConvertedRegionPrice = ['DE' => new ConvertedRegionPrice('DE',
-            new Money('EUR', '10', 990000000),
-            new Money('EUR', '1', 760000000),
-        ), 'JP' => new ConvertedRegionPrice('JP',
-            new Money('JPY', '1480', 0),
-            new Money('JPY', '135', 0),
-        )];
-        $this->assertEquals($ConvertedRegionPrice, $actual->convertedRegionPrices);
+        $convertedRegionPrices = [
+            'DE' => new ConvertedRegionPrice(
+                regionCode: 'DE',
+                price: new Money(currencyCode: 'EUR', units: '10', nanos: 990000000),
+                taxAmount: new Money(currencyCode: 'EUR', units: '1', nanos: 760000000),
+            ),
+            'JP' => new ConvertedRegionPrice(
+                regionCode: 'JP',
+                price: new Money(currencyCode: 'JPY', units: '1480', nanos: 0),
+                taxAmount: new Money(currencyCode: 'JPY', units: '135', nanos: 0),
+            ),
+        ];
         $convertedOtherRegionsPrice = new ConvertedOtherRegionsPrice(
-            new Money('USD', '9', 990000000),
-            new Money('EUR', '9', 490000000),
+            usdPrice: new Money(currencyCode: 'USD', units: '9', nanos: 990000000),
+            eurPrice: new Money(currencyCode: 'EUR', units: '9', nanos: 490000000),
         );
+        $regionVersion = new RegionsVersion(version: '2024/02');
+        $this->assertEquals($convertedRegionPrices, $actual->convertedRegionPrices);
         $this->assertEquals($convertedOtherRegionsPrice, $actual->convertedOtherRegionsPrice);
-        $this->assertEquals('2024/02', $actual->regionVersion->version);
+        $this->assertEquals($regionVersion, $actual->regionVersion);
     }
 }
