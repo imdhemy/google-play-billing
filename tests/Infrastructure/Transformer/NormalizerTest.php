ded556cdc4f70209f13d42bd03d234634bf0870a
Taha Elsenosy <tahaelsenosy@gmail.com>
2026-01-17 11:18:08 +0200

    feat: add response interface test case for the normalizer

--- tests/Infrastructure/Transformer/NormalizerTest.php
+++ tests/Infrastructure/Transformer/NormalizerTest.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace Tests\Infrastructure\Transformer;
 
 use Imdhemy\GooglePlay\Infrastructure\Transformer\Normalizer;
+use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use Tests\TestCase;
 
 final class NormalizerTest extends TestCase
@@ -28,6 +32,24 @@ final class NormalizerTest extends TestCase
 
         $this->assertInstanceOf(BackedEnumExample::class, $instance);
         $this->assertEquals(1, $instance->value);
+    }
+
+    #[Test]
+    #[AllowMockObjectsWithoutExpectations]
+    public function it_normalizes_from_response_interface(): void
+    {
+        $data = ['name' => $this->faker->name()];
+        $body = json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR);
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getBody')->willReturn($this->createMock(StreamInterface::class));
+        /** @var MockObject $stream */
+        $stream = $response->getBody();
+        $stream->method('getContents')->willReturn($body);
+
+        $instance = Normalizer::create()->normalize($response, MyValueObject::class);
+
+        $this->assertInstanceOf(MyValueObject::class, $instance);
+        $this->assertEquals($data['name'], $instance->name);
     }
 }
 
