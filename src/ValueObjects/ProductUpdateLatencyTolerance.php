51c507c6028ba53046b7b46723f6256d88ddf812
Eman Fateen <emanfateen@gmail.com>
2026-04-26 15:46:58 +0300

    chore: introduce ProductUpdateLatencyTolerance enum (#283)

---
+++ src/ValueObjects/ProductUpdateLatencyTolerance.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\GooglePlay\ValueObjects;
+
+/**
+ * @see https://developers.google.com/android-publisher/api-ref/rest/v3/ProductUpdateLatencyTolerance
+ */
+enum ProductUpdateLatencyTolerance: string
+{
+    case UNSPECIFIED = 'PRODUCT_UPDATE_LATENCY_TOLERANCE_UNSPECIFIED';
+    case SENSITIVE = 'PRODUCT_UPDATE_LATENCY_TOLERANCE_LATENCY_SENSITIVE';
+    case TOLERANT = 'PRODUCT_UPDATE_LATENCY_TOLERANCE_LATENCY_TOLERANT';
+}
